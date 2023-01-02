7feb6c80c2e21615444ee22f5b2a622d25a4667c
Mário Santos <mariodev7@gmail.com>
2023-01-02 00:26:28 -0300

    refactor(sitemap): disable generate sitemap index

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
 module.exports = {
   siteUrl,
+
   generateRobotsTxt: true,
   exclude: ["/404"],
   robotsTxtOptions: {
@@ -11,9 +12,8 @@ module.exports = {
       { userAgent: "*", allow: "/" },
       { userAgent: "*", disallow: "/404" },
     ],
-    additionalSitemaps: [
-      `${siteUrl}/sitemap.xml`,
-      `${siteUrl}/server-sitemap.xml`,
-    ],
+
+    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
   },
+  generateIndexSitemap: false,
 };
